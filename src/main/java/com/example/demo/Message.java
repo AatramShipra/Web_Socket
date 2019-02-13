50365d82ec52955ee23e0a1c3a327a6e211a437d
Your Name <you@example.com>
2019-02-13 18:02:50 +0530

    done

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Message {
+
+    private  String name;
+
+    public Message() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Message(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
